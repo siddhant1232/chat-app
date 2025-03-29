45305447d43558f1070f9fea2549204a99f8ccca
siddhant1232 <siddhantgupta797400@gmail.com>
2025-03-30 01:12:04 +0530

    updated message cotroller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js"
+import Message from '../models/message.models.js'
 
 export const getUsersForSidebar = async (req,res)=>{  
   try {
@@ -12,5 +13,48 @@ export const getUsersForSidebar = async (req,res)=>{
   }
 }
 export const getAllmessages = async (req,res)=>{
+  try {
+    const {id:chatWithUserId} = req.params;
+    const myId = req.User._id;
 
+    const messages = await Message.find({
+      $or:[
+        {senderId:myId , recieverId : chatWithUserId},
+        {senderId: chatWithUserId , recieverId: myId},
+      ]
+    });
+
+    res.status(200).json(messages)
+  } catch (error) {
+    console.log('error in getallmessage controller',error.messsage);
+    res.status(500).json({messsage:'internal server error'});
+  }
 }
+
+export const sendMessage = async (req,res) =>{
+ try {
+  const {text,image} = req.body;
+  console.log(req.body); 
+
+  const {id:recieverId} = req.params;
+  const senderId = req.User._id;
+
+  let imageUrl;
+  if(image){
+    const uploadresponse = await cloudinary.uploader.upload(image);
+    imageUrl = uploadresponse.secure_url;
+  }
+
+  const newMessage = new Message({
+    senderId,recieverId,text,image:imageUrl,
+  });
+
+  await newMessage.save();
+
+  res.status(201).json(newMessage);
+ } catch (error) {
+  console.log('error in sendMessage controller',error.message);
+  res.status(500).json({message:'Internal server error'});
+ }
+}
+  
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { privateRoute } from '../middlewares/auth.middleware.js';
-import {getUsersForSidebar,getAllmessages} from '../controllers/message.controller.js'
+import {getUsersForSidebar,getAllmessages,sendMessage} from '../controllers/message.controller.js'
 
 
 const router = express.Router();
@@ -8,4 +8,5 @@ const router = express.Router();
 router.get('/users',privateRoute,getUsersForSidebar);
 router.get('/:id',privateRoute,getAllmessages)
 
+router.post('/send/:id',privateRoute,sendMessage)
 export default router;
