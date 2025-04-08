96ce95de2fe3ae89ab09e4aadf834dfc5a40cc62
siddhant1232 <siddhantgupta797400@gmail.com>
2025-04-09 00:00:48 +0530

    pages updated

--- frontend/src/pages/signUpPage.tsx
+++ frontend/src/pages/signUpPage.tsx
@@ -1,22 +1,22 @@
-import { useAuthStore } from '@/store/authStore';
-import { MessageSquare, EyeOff ,Mail , Loader2 ,Eye ,Lock ,User } from 'lucide-react';
-import { useState } from 'react'
-import { Link } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import AuthImagePattern from '@/components/AuthImagePattern';
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
+import { Link } from "react-router-dom";
 
-const signUpPage = () => {
+import AuthImagePattern from "../components/AuthImagePattern";
+import toast from "react-hot-toast";
 
+const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const [formData , setFormData] = useState({
-    fullName:"",
-    email:"",
-    password:"",
-  })
-  
-  const {signUp,isSigningUp} = useAuthStore();
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
 
-  const validateForm =()=>{
+  const { signup, isSigningUp } = useAuthStore();
+
+  const validateForm = () => {
     if (!formData.fullName.trim()) return toast.error("Full name is required");
     if (!formData.email.trim()) return toast.error("Email is required");
     if (!/\S+@\S+\.\S+/.test(formData.email)) return toast.error("Invalid email format");
@@ -24,30 +24,35 @@ const signUpPage = () => {
     if (formData.password.length < 6) return toast.error("Password must be at least 6 characters");
 
     return true;
-  }
+  };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+
     const success = validateForm();
 
-    if (success === true) signUp();
-  }
+    if (success === true) signup(formData);
+  };
+
   return (
     <div className="min-h-screen grid lg:grid-cols-2">
+      {/* left side */}
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
-          {/* logo */}
+          {/* LOGO */}
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
-              <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center
-              group-hover:bg-primary/20 transition-colors">
-                <MessageSquare className="size-6 text-primary"/>
+              <div
+                className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
+              group-hover:bg-primary/20 transition-colors"
+              >
+                <MessageSquare className="size-6 text-primary" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Create Account</h1>
-              <p className="text-base-content/60">Get stated with your free account</p>
+              <p className="text-base-content/60">Get started with your free account</p>
             </div>
           </div>
-          {/* form */}
+
           <form onSubmit={handleSubmit} className="space-y-6">
             <div className="form-control">
               <label className="label">
@@ -134,15 +139,16 @@ const signUpPage = () => {
               </Link>
             </p>
           </div>
-        
         </div>
       </div>
+
+      {/* right side */}
+
       <AuthImagePattern
         title="Join our community"
         subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
       />
     </div>
   );
-}
-
-export default signUpPage
+};
+export default SignUpPage;
