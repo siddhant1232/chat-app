e3c2c6f5960e6b218382d78d909b5bc2445fb43a
siddhant1232 <siddhantgupta797400@gmail.com>
2025-04-09 00:45:57 +0530

    project success

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,42 @@
 import express from "express";
-import authroutes from './routes/auth.route.js';
-import dotenv from 'dotenv';
-import { connectDB } from "./lib/db.js";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import messageroutes from "./routes/message.route.js"
-import cors from 'cors';
+import cors from "cors";
 
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-const app = express();
-const PORT = process.env.PORT || 5001;
-
-app.use(cookieParser())
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials:true,
-}));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use('/api/auth', authroutes);
-app.use('/api/message',messageroutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.listen(PORT,()=>{
-  console.log(`server is running at port ${PORT}`)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
   connectDB();
-})
+});
