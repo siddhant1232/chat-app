ccebcbe21cb5261ca66c0f43c44367289aeaa638
siddhant1232 <siddhantgupta797400@gmail.com>
2025-04-12 17:54:33 +0530

    deployment phase-01

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 
 import HomePage from "@/pages/HomePage";
 import SignUpPage from "@/pages/SignUpPage";
-import LoginPage from "@/pages/LogInPage";
+import LoginPage from "@/pages/LoginPage";
 import SettingsPage from "@/pages/SettingsPage";
 import ProfilePage from "@/pages/ProfilePage";
 
