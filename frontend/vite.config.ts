77151d6ff5d17a279811ce290ce15ebd9d9c1b04
siddhant1232 <siddhantgupta797400@gmail.com>
2025-04-12 21:51:32 +0530

    hard

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  // Removed invalid 'path' property
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
