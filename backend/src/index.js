9328882b40eacf86314a9c0f09e6ba4335baf5da
siddhant1232 <siddhantgupta797400@gmail.com>
2025-04-14 23:48:29 +0530

    ram

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-1-flame.vercel.app/",
     credentials: true,
   })
 );
